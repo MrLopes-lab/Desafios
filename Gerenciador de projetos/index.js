5d8cffdf3ca5afd00277dbe5927c9148d30a7ac8
Marcelo Lopes Rodrigues <lopesrodrigues05@gmail.com>
2019-10-23 15:47:43 -0300

    commit

---
+++ Gerenciador de projetos/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const server = express()
+
+const projects = [{
+    id: "1",
+    title: "Novo projeto",
+    tasks: ["Nova tarefa"]
+  }]
+
+// CRIAR PROJETO
+server.post('/projects', (req, res) => {
+    const { id } = req.body
+
+
+    return res.json(projects[id])
+})
+
+
+server.listen(3000)
