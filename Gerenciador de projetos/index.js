22ef17b46cd1a8668abb60a704b2076372950044
Marcelo Lopes Rodrigues <lopesrodrigues05@gmail.com>
2019-10-24 16:04:10 -0300

    atualização

--- Gerenciador de projetos/index.js
+++ Gerenciador de projetos/index.js
@@ -3,26 +3,38 @@ const server = express()
 
 server.use(express.json())
 
-const projects = [{
-    id: "1",
-    title: "Site1",
-    tasks: ["Nova tarefa"]
-  }]
+const projects = []
 
 // CRIAR PROJETO
 server.post('/projects', (req, res) => {
     const { id } = req.body
     const { title } = req.body
     
-    projects.push({"id": id, "title": title})
+    projects.push({"id": id, "title": title, "tasks": []})
 
     return res.json(projects)
 })
-
 
 // LISTAR PROJETOS
 server.get('/projects', (req, res) => {
   return res.json(projects)
 })
 
-server.listen(3000)
+// ALTERAR TITLE
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params
+  const { title } = req.body
+
+  const project = projects.find(p => p.id == id)
+
+  project.title = title
+  
+  return res.json(projects)
+})
+
+// DELETAR UM PROJETO
+server.delete('/projects/:id'), (req, res) => {
+  const { id } = req.params
+}
+
+server.listen(3030)
