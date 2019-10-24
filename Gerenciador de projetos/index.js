ed06fdbce5e43b669f0ce6e0052923a08b00677b
MrDev <lopesrodrigues05@gmail.com>
2019-10-23 21:44:44 -0300

    commit

--- Gerenciador de projetos/index.js
+++ Gerenciador de projetos/index.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const server = express()
 
+server.use(express.json())
+
 const projects = [{
     id: "1",
-    title: "Novo projeto",
+    title: "Site1",
     tasks: ["Nova tarefa"]
   }]
 
 // CRIAR PROJETO
 server.post('/projects', (req, res) => {
     const { id } = req.body
+    const { title } = req.body
+    
+    projects.push({"id": id, "title": title})
 
-
-    return res.json(projects[id])
+    return res.json(projects)
 })
 
-//ola teste
+
+// LISTAR PROJETOS
+server.get('/projects', (req, res) => {
+  return res.json(projects)
+})
+
 server.listen(3000)
